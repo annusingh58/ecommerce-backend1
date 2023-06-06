c0352520e4066d7ffc0dea0b479be086e5621854
anu singh <annusingh58@gmail.com>
2023-06-06 11:00:47 +0530

    updates

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -6,10 +6,17 @@ export const addProduct = async (req, res) => {
 
         const { email, name, price } = req.body;
         if (!email) return res.send("Email is required!")
+        if(!name) return res.send("Name is required")
+        if(!price) return res.send("price is required")
+
+
         const user = await user.find({ email }).exec();
         console.log(user[0])
-        if (!user.length) return res.send("User not found!")
-        const products= ({name:name,
+        if (!user.length) 
+        return res.send("User not found!")
+        
+        const products= ({
+                        name:name,
                     price:price});
         user[0].products.push(products);
     
